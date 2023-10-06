9aa0e340a6ed0e79a712bae2ee8414a89973a9bb
Ubuntu User <gerardo@croney.io>
2023-10-06 14:40:03 -0400

    Fix descargausuario4

--- querydescargausuario4.sh
+++ querydescargausuario4.sh
@@ -18,7 +18,7 @@ cd $PrPWD
 PrPWD2=$PWD
 PrPWD=$PrPWD2
 cd $PaPWD
-remotepath="https://curare2019.ddns.net/"
+remotepath="http://127.0.0.1/" #"https://curare2019.ddns.net/"
 lista0=$(curl -L "$remotepath/dirlistmt.php" 2>/dev/null | tr '
 ' ' ' )
 
@@ -36,7 +36,9 @@ while [ -n "$slash" ];do
     pn=$(echo "$pn" | $PrPWD/stdcdr "/" )
     slash=$(echo $pn | $PrPWD/stdbuscaarg_donde_hasta "/" )
 done
-dondes=$(echo "$lista0" | $PrPWD/stdbuscaarg_donde " ")
+lista0=$(echo "$lista0 ")
+dondes=$(echo "$lista0" | tr '
+' ' '| $PrPWD/stdbuscaarg_donde " ")
 pren=0
 while [ -n "$continua" ];do
     ii=$(expr $i + 300)
@@ -50,7 +52,7 @@ while [ -n "$continua" ];do
 	    n=$(echo "$dondes" | $PrPWD/stdcarsin ' ' )
 	    n2=$(echo 0$n - $pren)
 	    listf=$(echo "$lista0" | $PrPWD/stdcdrn 0$pren |  $PrPWD/stdcarsin ' ' )
-#	    echo $pren $n $listf
+	    echo $pren $n $listf
 	    pren=$(expr 0$n + 1)
 	    dondes=$(echo "$dondes" | $PrPWD/stdcdr ' ' )
             echo "$listf" >> "$PaPWD/$pn.l.$ii"
@@ -125,166 +127,165 @@ while [ -n "$lista0" ];do
 		if [ -z "$opens" -o "0$opens" -eq 0 ];then
 		    if [ "0$size" -gt 30 -o "0$size" -lt 18 ];then
 			echo ";$listf;" > "$fn.memoria"
-			echo ">>$size $opens $closs"
-		    else			
-			balan=$(expr 0$opens - 0$closs )
-			    echo -n "$size:"
-			if [ 0$opens -gt 0 -a "$balan" = "0"  ];then
-			    echo "> $opens : $closs < $balan $fn"
-			    echo ";$listf;" > $fn.memoria
-			    cat "$fn" | gpg  --homedir $PrPWD/user/ --no-default-keyring --keyring $PrPWD/user/key.key --secret-keyring $PrPWD/user/key.gpg --trustdb-name $PrPWD/user/trustdb.gpg  -d  2>/dev/null 1>$fn.c
-			    balan=$(cat "$fn.c"|wc -c|$PrPWD/stddelcar " " )
-			    if [ 0$balan -gt 0 ];then
-				echo '/*' | tr -d '
+			echo ">> >> >> $size $opens $closs"
+		    fi
+		else			
+		    balan=$(expr 0$opens - 0$closs )
+		    echo "> $opens : $closs < $balan $fn )"
+		    if [ 0$opens -gt 0 -a "$balan" = "0"  ];then
+			echo ";$listf;" > $fn.memoria
+			cat "$fn" | gpg  --homedir $PrPWD/user/ --no-default-keyring --keyring $PrPWD/user/key.key --secret-keyring $PrPWD/user/key.gpg --trustdb-name $PrPWD/user/trustdb.gpg  -d  2>/dev/null 1>$fn.c
+			balan=$(cat "$fn.c"|wc -c|$PrPWD/stddelcar " " )
+			if [ 0$balan -gt 0 ];then
+			    echo '/*' | tr -d '
 ' > $fn.tmp
-				cat $fn.c >> $fn.tmp
-				mv -v $fn.tmp $fn.c
-				mains=$(cat "$fn.c"|$PrPWD/stdbuscaarg " main")
-				opens=$(cat "$fn.c"|$PrPWD/stdcdr " main"|$PrPWD/stdbuscaarg_count "{")
-				closs=$(cat "$fn.c"|$PrPWD/stdcdr " main"|$PrPWD/stdbuscaarg_count "}")
-				balan=$(expr  0$opens - 0$closs)
-				echo ">>> $opens : $closs <<< $balan ($mains)"
-				if [ 0$opens -gt 0 -a "$balan" = "0" -a -n "$mains" ];then
-    				    errors=$(gcc $fn.c 2>&1)
-				    if [ -n "$errors" ];then
-					echo "There are errors on $fn.c"
-				    fi
-				    #echo "$errors"
-				    mkdir peticiones
-				    if [ -z "$errors" ];then
-					variables=$(cat $fn.c |$PrPWD/stddeclaracionesdevariable|tr '
+			    cat $fn.c >> $fn.tmp
+			    mv -v $fn.tmp $fn.c
+			    mains=$(cat "$fn.c"|$PrPWD/stdbuscaarg " main")
+			    opens=$(cat "$fn.c"|$PrPWD/stdcdr " main"|$PrPWD/stdbuscaarg_count "{")
+			    closs=$(cat "$fn.c"|$PrPWD/stdcdr " main"|$PrPWD/stdbuscaarg_count "}")
+			    balan=$(expr  0$opens - 0$closs)
+			    echo ">>> $opens : $closs <<< $balan ($mains)"
+			    if [ 0$opens -gt 0 -a "$balan" = "0" -a -n "$mains" ];then
+    				errors=$(gcc $fn.c 2>&1)
+				if [ -n "$errors" ];then
+				    echo "There are errors on $fn.c"
+				fi
+				#echo "$errors"
+				mkdir peticiones
+				if [ -z "$errors" ];then
+				    variables=$(cat $fn.c |$PrPWD/stddeclaracionesdevariable|tr '
 ' ';')
-					userfromfile=$(echo ";$variables" |$PrPWD/stdcdr ";char *"|$PrPWD/stdcarsin "=")
-					ttest=$(echo ";$variables" |$PrPWD/stdbuscaarg_donde_hasta ";int ")
-					#				   echo "<<< ttest $ttest >>>"
-					dondes=0
-					while [ "0$ttest" -gt 0 ];do
-					    dondes=$(($dondes+$ttest))
-					    ivfromfile=$(echo ";$variables" |$PrPWD/stdcdrn $dondes|$PrPWD/stdcarsin ";")
-					    echo "-> $ivfromfile"
-					    len=$(echo $ivfromfile|$PrPWD/stdcdr "]"|$PrPWD/stdbuscaarg_donde_hasta "[16]")
-					    len2=$(echo $ivfromfile|$PrPWD/stdbuscaarg_count "][")
-					    if [ 0$len -gt 0 -a 0$len2 -eq 1 ];then
-						ivfromfile=$(echo "$ivfromfile"|$PrPWD/stdcarsin "[16]"|$PrPWD/stdcarsin "[")
-						break;
-					    else
-						ivfromfile=""
-					    fi
-					    ttest=$(echo ";$variables" |$PrPWD/stdcdrn $dondes|$PrPWD/stdbuscaarg_donde_hasta ";int ")
-					    echo "<<< ttest $ttest d $dondes>>>"
-					done
-					ttest=$(echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"|$PrPWD/stdbuscaarg_donde_hasta ";int ")
-					echo "________________________________________________________"
-					#				   echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"
-					echo "......................................................."
-					echo "<<< ttest $ttest >>>"
-					dondes=0
-					while [ "0$ttest" -gt 0 ];do
-					    dondes=$(($dondes+$ttest))
-					    encryptedfromfile=$(echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"|$PrPWD/stdcdrn $dondes|$PrPWD/stdcarsin ";")
-					    len2=$(echo $encryptedfromfile|$PrPWD/stdbuscaarg_count "][")
-					    echo "($len2)> $encryptedfromfile"
-					    if [ 0$len2 -eq 1 ];then
-						encryptedfromfile=$(echo "$encryptedfromfile"|$PrPWD/stdcarsin ";"|$PrPWD/stdcarsin "[")
-						break;
-					    else
-						encryptedfromfile=""
-					    fi
-					    ttest=$(echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"|$PrPWD/stdcdrn $dondes|$PrPWD/stdbuscaarg_donde_hasta ";int ")
-					    echo "<<< ttest $ttest d $dondes>>>"
-					done
-					filedfromfile=$(echo ";$variables" |$PrPWD/stdcdr ";FILE *"|$PrPWD/stdcarsin "="|$PrPWD/stdcarsin ";")
-					ca5=$(echo "$userfromfile $ivfromfile $encryptedfromfile $filedfromfile"|$PrPWD/stdbuscaarg " 0")
-					echo "<USER:$userfromfile> <IV:$ivfromfile> <ENC:$encryptedfromfile> <FILE:$filedfromfile> <CA:$ca5>"
-					if [ -n "$userfromfile" -a -n "$ivfromfile" -a -n "$encryptedfromfile" -a -n "$filedfromfile" -a -z "$ca5" ];then
-					    cat $fn.c |$PrPWD/stdcdr "*$userfromfile=" # |$PrPWD/stddelcar '"'
-					    usuarioo=$(echo ";$variables" |$PrPWD/stdcdr "$userfromfile=" |$PrPWD/stddelcar '"' | $PrPWD/stdcarsin ";");
-					    echo "uU $usuarioo Uu $userfromfile uU"
-					    if [ -f "$PrPWD/users/$usuarioo/$usuarioo-aes.c" ];then
+				    userfromfile=$(echo ";$variables" |$PrPWD/stdcdr ";char *"|$PrPWD/stdcarsin "=")
+				    ttest=$(echo ";$variables" |$PrPWD/stdbuscaarg_donde_hasta ";int ")
+				    #				   echo "<<< ttest $ttest >>>"
+				    dondes=0
+				    while [ "0$ttest" -gt 0 ];do
+					dondes=$(($dondes+$ttest))
+					ivfromfile=$(echo ";$variables" |$PrPWD/stdcdrn $dondes|$PrPWD/stdcarsin ";")
+					echo "-> $ivfromfile"
+					len=$(echo $ivfromfile|$PrPWD/stdcdr "]"|$PrPWD/stdbuscaarg_donde_hasta "[16]")
+					len2=$(echo $ivfromfile|$PrPWD/stdbuscaarg_count "][")
+					if [ 0$len -gt 0 -a 0$len2 -eq 1 ];then
+					    ivfromfile=$(echo "$ivfromfile"|$PrPWD/stdcarsin "[16]"|$PrPWD/stdcarsin "[")
+					    break;
+					else
+					    ivfromfile=""
+					fi
+					ttest=$(echo ";$variables" |$PrPWD/stdcdrn $dondes|$PrPWD/stdbuscaarg_donde_hasta ";int ")
+					echo "<<< ttest $ttest d $dondes>>>"
+				    done
+				    ttest=$(echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"|$PrPWD/stdbuscaarg_donde_hasta ";int ")
+				    echo "________________________________________________________"
+				    #				   echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"
+				    echo "......................................................."
+				    echo "<<< ttest $ttest >>>"
+				    dondes=0
+				    while [ "0$ttest" -gt 0 ];do
+					dondes=$(($dondes+$ttest))
+					encryptedfromfile=$(echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"|$PrPWD/stdcdrn $dondes|$PrPWD/stdcarsin ";")
+					len2=$(echo $encryptedfromfile|$PrPWD/stdbuscaarg_count "][")
+					echo "($len2)> $encryptedfromfile"
+					if [ 0$len2 -eq 1 ];then
+					    encryptedfromfile=$(echo "$encryptedfromfile"|$PrPWD/stdcarsin ";"|$PrPWD/stdcarsin "[")
+					    break;
+					else
+					    encryptedfromfile=""
+					fi
+					ttest=$(echo ";$variables"| $PrPWD/stddelcar "int $ivfromfile"|$PrPWD/stdcdrn $dondes|$PrPWD/stdbuscaarg_donde_hasta ";int ")
+					echo "<<< ttest $ttest d $dondes>>>"
+				    done
+				    filedfromfile=$(echo ";$variables" |$PrPWD/stdcdr ";FILE *"|$PrPWD/stdcarsin "="|$PrPWD/stdcarsin ";")
+				    ca5=$(echo "$userfromfile $ivfromfile $encryptedfromfile $filedfromfile"|$PrPWD/stdbuscaarg " 0")
+				    echo "<USER:$userfromfile> <IV:$ivfromfile> <ENC:$encryptedfromfile> <FILE:$filedfromfile> <CA:$ca5>"
+				    if [ -n "$userfromfile" -a -n "$ivfromfile" -a -n "$encryptedfromfile" -a -n "$filedfromfile" -a -z "$ca5" ];then
+					cat $fn.c |$PrPWD/stdcdr "*$userfromfile=" # |$PrPWD/stddelcar '"'
+					usuarioo=$(echo ";$variables" |$PrPWD/stdcdr "$userfromfile=" |$PrPWD/stddelcar '"' | $PrPWD/stdcarsin ";");
+					echo "uU $usuarioo Uu $userfromfile uU"
+					if [ -f "$PrPWD/users/$usuarioo/$usuarioo-aes.c" ];then
+					    outputdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
+					    while [ -f "$outputdelc.c" ];do
 						outputdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
-						while [ -f "$outputdelc.c" ];do
-						    outputdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
-						done
-						echo "oOO $outputdelc OOo"
-						arrayindex=$(cat $fn.c | $PrPWD/stdcdr "int$ivfromfile["  | $PrPWD/stdcarsin "]");
-						dondes=0
-						iv=$(echo "$variables" | $PrPWD/stdcdr "int$ivfromfile["  | $PrPWD/stdcarsin ";");
-						msj=$(echo "$variables" | $PrPWD/stdcdr "int$encryptedfromfile["|$PrPWD/stdcarsin ";");
-						filed=$(cat $fn.c | $PrPWD/stdcdr "$filedfromfile=" | $PrPWD/stdcdr "fopen(" |$PrPWD/stddelcar '"' | $PrPWD/stdcarsin ",");
-						c=0
-						tail $PrPWD/users/$usuarioo/$usuarioo-aes.c
-						echo "IV $ivfromfile $iv"
-						cat $PrPWD/users/$usuarioo/$usuarioo-aes.c | $PrPWD/stdcar "unsigned char iv[" > $outputdelc.c
-						echo "$iv;" >> $outputdelc.c
-						cat $PrPWD/users/$usuarioo/$usuarioo-aes.c | $PrPWD/stdcdr "unsigned char iv[" | $PrPWD/stdcdr ";" >> $outputdelc.c
+					    done
+					    echo "oOO $outputdelc OOo"
+					    arrayindex=$(cat $fn.c | $PrPWD/stdcdr "int$ivfromfile["  | $PrPWD/stdcarsin "]");
+					    dondes=0
+					    iv=$(echo "$variables" | $PrPWD/stdcdr "int$ivfromfile["  | $PrPWD/stdcarsin ";");
+					    msj=$(echo "$variables" | $PrPWD/stdcdr "int$encryptedfromfile["|$PrPWD/stdcarsin ";");
+					    filed=$(cat $fn.c | $PrPWD/stdcdr "$filedfromfile=" | $PrPWD/stdcdr "fopen(" |$PrPWD/stddelcar '"' | $PrPWD/stdcarsin ",");
+					    c=0
+					    tail $PrPWD/users/$usuarioo/$usuarioo-aes.c
+					    echo "IV $ivfromfile $iv"
+					    cat $PrPWD/users/$usuarioo/$usuarioo-aes.c | $PrPWD/stdcar "unsigned char iv[" > $outputdelc.c
+					    echo "$iv;" >> $outputdelc.c
+					    cat $PrPWD/users/$usuarioo/$usuarioo-aes.c | $PrPWD/stdcdr "unsigned char iv[" | $PrPWD/stdcdr ";" >> $outputdelc.c
 
-						cat $outputdelc.c | $PrPWD/stdcar "unsigned char buf[" > $outputdelc-2.c
-						echo "$msj;" >> $outputdelc-2.c
-						cat $outputdelc.c | $PrPWD/stdcdr "unsigned char buf[" | $PrPWD/stdcdr ";" >> $outputdelc-2.c
-						mv $outputdelc-2.c $outputdelc.c
+					    cat $outputdelc.c | $PrPWD/stdcar "unsigned char buf[" > $outputdelc-2.c
+					    echo "$msj;" >> $outputdelc-2.c
+					    cat $outputdelc.c | $PrPWD/stdcdr "unsigned char buf[" | $PrPWD/stdcdr ";" >> $outputdelc-2.c
+					    mv $outputdelc-2.c $outputdelc.c
 
-						echo "$outputdelc.c"
-						tail $outputdelc.c
-						errores=$(gcc -o $outputdelc-bin $outputdelc.c 2>&1)
-						echo "< < < $errores > > >"
-						if [ -z "$errores" ];then
-						    datosdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
-						    while [ -f "$datosdelc.c" ];do
-							datosdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
-						    done
-						    $PaPWD/$outputdelc-bin
-						    echo "$datosdelc.c"
-						    echo "==================="
-						    echo  "/*" |tr -d '
-' > $datosdelc.c
-						    $PaPWD/$outputdelc-bin | $PrPWD/stdcdr "buf[" | $PrPWD/stdcarsin ";" | $PrPWD/stdfromdec  >> $datosdelc.c
-						    cat "$datosdelc.c"
-
-						    errores=$(gcc -o $datosdelc-bin $datosdelc.c 2>&1)
-						    echo ">>>> '!!!!!'          $errores <<<<"
-						    if [ -z "$errores" ];then
-							mkdir $PrPWD/users
-							mkdir $PrPWD/users/input
-							mkdir $PrPWD/users/input/$usuarioo
-							cp -v $datosdelc.c $PrPWD/users/input/$usuarioo/
-						    fi
-						fi
-					    else
+					    echo "$outputdelc.c"
+					    tail $outputdelc.c
+					    errores=$(gcc -o $outputdelc-bin $outputdelc.c 2>&1)
+					    echo "< < < $errores > > >"
+					    if [ -z "$errores" ];then
 						datosdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
 						while [ -f "$datosdelc.c" ];do
 						    datosdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
 						done
-						outputdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
-						while [ -f "$outputdelc.c" ];do
-						    outputdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
-						done
-						echo "Error de usuario de nombre de usuario o contraseña"
-						echo "errores=\"Error de usuario de nombre de usuario o contraseña\";" > $outputdelc
-						echo "processed=255;" >> $outputdelc
+						$PaPWD/$outputdelc-bin
+						echo "$datosdelc.c"
+						echo "==================="
+						echo  "/*" |tr -d '
+' > $datosdelc.c
+						$PaPWD/$outputdelc-bin | $PrPWD/stdcdr "buf[" | $PrPWD/stdcarsin ";" | $PrPWD/stdfromdec  >> $datosdelc.c
+						cat "$datosdelc.c"
+
+						errores=$(gcc -o $datosdelc-bin $datosdelc.c 2>&1)
+						echo ">>>> '!!!!!'          $errores <<<<"
+						if [ -z "$errores" ];then
+						    mkdir $PrPWD/users
+						    mkdir $PrPWD/users/input
+						    mkdir $PrPWD/users/input/$usuarioo
+						    cp -v $datosdelc.c $PrPWD/users/input/$usuarioo/
+						fi
 					    fi
-					    if [ -n "$errores" ];then
-						echo "errores=\"Error de usuario de nombre de usuario o contraseña\";" > $outputdelc
-						echo "processed=255;" >> $outputdelc
-					    else
-						echo "errores=\"Entrada registrada correctamente, en cuanto se encunetre procesada aparecera la respuesta en la lista de mensajes (con el titulo indicado en la entrada)\";" > $outputdelc
-						echo "processed=255;" >> $outputdelc
-					    fi
-					    echo "encrypted="'`'|tr -d '
-' > $datosdelc.js
-					    cat $outputdelc | gpg -a --no-default-keyring --keyring $PrPWD/user/key.key  --sign  >> $datosdelc.js
-					    echo '`'";"  >> $datosdelc.js
-					    echo "processed=255;"  >> $datosdelc.js
-					    #curl --retry-delay 5 --retry 30 --retry-all-errors --socks5-hostname 127.0.0.1:9050 --insecure  --user "$usuarioftp:$passwordftp" -T $datosdelc.js "ftp://$remotepath/panal/msgs/$filed"
-					    cat $datosdelc.js
 					else
-					    echo "======================="
-					    mkdir $PrPWD/users
-					    mkdir $PrPWD/users/input
-					    mkdir $PrPWD/users/input/unencrypted
-					    cp -v $datosdelc.c $PrPWD/users/input/$usuarioo/
-					    cp -v "$fn.c" "$PrPWD/users/input/unencrypted"
-					    echo "$errores"
+					    datosdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
+					    while [ -f "$datosdelc.c" ];do
+						datosdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
+					    done
+					    outputdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
+					    while [ -f "$outputdelc.c" ];do
+						outputdelc=$(dd if=/dev/random bs=1 count=10 2>/dev/null |$PrPWD/stdtohex|$PrPWD/stddelcar " ")
+					    done
+					    echo "Error de usuario de nombre de usuario o contraseña"
+					    echo "errores=\"Error de usuario de nombre de usuario o contraseña\";" > $outputdelc
+					    echo "processed=255;" >> $outputdelc
 					fi
+					if [ -n "$errores" ];then
+					    echo "errores=\"Error de usuario de nombre de usuario o contraseña\";" > $outputdelc
+					    echo "processed=255;" >> $outputdelc
+					else
+					    echo "errores=\"Entrada registrada correctamente, en cuanto se encunetre procesada aparecera la respuesta en la lista de mensajes (con el titulo indicado en la entrada)\";" > $outputdelc
+					    echo "processed=255;" >> $outputdelc
+					fi
+					echo "encrypted="'`'|tr -d '
+' > $datosdelc.js
+					cat $outputdelc | gpg -a --no-default-keyring --keyring $PrPWD/user/key.key  --sign  >> $datosdelc.js
+					echo '`'";"  >> $datosdelc.js
+					echo "processed=255;"  >> $datosdelc.js
+					#curl --retry-delay 5 --retry 30 --retry-all-errors --socks5-hostname 127.0.0.1:9050 --insecure  --user "$usuarioftp:$passwordftp" -T $datosdelc.js "ftp://$remotepath/panal/msgs/$filed"
+					cat $datosdelc.js
+				    else
+					echo "======================="
+					mkdir $PrPWD/users
+					mkdir $PrPWD/users/input
+					mkdir $PrPWD/users/input/unencrypted
+					cp -v $datosdelc.c $PrPWD/users/input/$usuarioo/
+					cp -v "$fn.c" "$PrPWD/users/input/unencrypted"
+					echo "$errores"
 				    fi
 				fi
 			    fi
@@ -296,7 +297,7 @@ while [ -n "$lista0" ];do
 	if [ -f "$fn.lock" ];then
 	    rm -v "$fn.lock" 2>/dev/null
 	fi
-    fi
+    fi    
 done
 rm -v $PaPWD/$pn.l.$ii
 echo "!"
